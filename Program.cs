fdde6e863249c70e967d153d3540849c3617c1b6
Neto-Pereira25 <devneto203@gmail.com>
2025-09-21 00:23:14 -0300

    chore: update de pacotes NuGet para versões estáveis

--- Program.cs
+++ Program.cs
@@ -5,8 +5,11 @@ using TrilhaApiDesafio.Context;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+builder.Services.AddDbContext<OrganizadorContext>(options => options.UseMySql(
+        builder.Configuration.GetConnectionString("ConexaoPadrao"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("ConexaoPadrao"))
+    )
+);
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
